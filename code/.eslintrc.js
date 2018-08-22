fb47620e0d9050a7a6d39ca3fe5dfd5dde58e771
ryan bruun <ryan.bruun@cloudreach.com>
2018-08-22 14:43:27 -0700

    Added boat.js and .eslintrc.js

---
+++ code/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
